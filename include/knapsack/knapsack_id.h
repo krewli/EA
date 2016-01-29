ffb2e4e29ad551b7463f943579593e5328d85ff8
krewli <krewli@hotmail.com>
2016-01-29 21:22:53 +0100

    Added code for constructing a knapsack and data files for knapsack problems

---
+++ include/knapsack/knapsack_id.h
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   knapsack_id.h
+ * Author: krewli
+ *
+ * Created on January 29, 2016, 9:03 PM
+ */
+
+#ifndef KNAPSACK_ID_H
+#define KNAPSACK_ID_H
+
+namespace Knapsack
+{
+    class KnapsackID
+    {
+    public:
+	KnapsackID(int id)
+	: id_{ id }
+	{
+	}
+
+	KnapsackID(const KnapsackID&) = default;
+
+	~KnapsackID() = default;
+
+	bool operator==(const KnapsackID& other) const
+	{
+	    return (id_ == other.id_);
+	}
+
+	bool operator!=(const KnapsackID& other) const
+	{
+	    return !(*this == other);
+	}
+
+    private:
+	const int id_;
+
+	KnapsackID(KnapsackID&&) = delete;
+
+	KnapsackID& operator=(const KnapsackID&) = delete;
+	KnapsackID& operator=(KnapsackID&&) = delete;
+    };
+}
+
+#endif /* KNAPSACK_ID_H */
+
---
+++ src/knapsack/knapsack_utilities.cpp
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+#include "knapsack_utilities.h"
+#include "knapsack_item.h"
+#include "knapsack_id.h"
+
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+
+namespace Knapsack
+{
+    Knapsack read_knapsack_data( const std::string& filename )
+    {
+	std::ifstream file { filename } ;
+
+	if ( !file )
+	{
+	    throw std::runtime_error("Could not open file " + filename + ".\n") ;
+	}
+
+	unsigned capacity ;
+	unsigned number_of_items ;
+
+	file >> number_of_items >> capacity ;
+
+	Knapsack::data_type items ;
+
+	int value, weight ;
+
+	for ( unsigned i = 0 ; i < number_of_items ; i++ )
+	{
+	    file >> value >> weight ;
+	    items.push_back(KnapsackItem { KnapsackID(i), value, weight }) ;
+	}
+
+	file.close() ;
+
+	return Knapsack { KnapsackID(0), capacity, number_of_items, items } ;
+    }
+}
