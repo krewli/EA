8fa11efafd1f979eb73268d562ad7c5ca01137f6
krewli <krewli@hotmail.com>
2016-02-05 18:09:43 +0100

    Added one more test for knapsack selection

--- test/knapsack/test_knapsack_selection.cpp
+++ test/knapsack/test_knapsack_selection.cpp
@@ -67,6 +67,41 @@ namespace knapsack
 		FAIL();
 	    }
 	}
+	
+	TEST_F( KnapsackSelectionTestSuite, test_selection_with_only_one_individual_having_fitness_above_0 )
+	{
+	    try
+	    {
+		Knapsack knapsack = read_knapsack("data/knapsack/ks_3_0");
+
+		util::Seed seed { util::NO_SEED };
+
+		knapsack_individual id1 { 3 };
+		knapsack_individual id2 { 3 };
+		knapsack_individual id3 { 3 };
+
+		knapsack_individual::chromosome_encoding chromosome1 { true, true, true };
+		knapsack_individual::chromosome_encoding chromosome2 { false, false, false };
+		knapsack_individual::chromosome_encoding chromosome3 { false, false, false };
+
+		id1.set_chromosome(chromosome1);
+		id2.set_chromosome(chromosome2);
+		id3.set_chromosome(chromosome3);
+
+		knapsack_generation_type generation { id1, id2, id3 };
+
+		knapsack_individual actual_result = KnapsackRouletteWheel::select(generation, knapsack, seed);
+
+		EXPECT_EQ(id1, actual_result);
+		EXPECT_NE(id2, actual_result);
+		EXPECT_NE(id3, actual_result);
+	    }
+	    catch ( std::runtime_error& e )
+	    {
+		std::cout << "Runtime error: " << e.what() << std::endl;
+		FAIL();
+	    }
+	}
     }
 }
 
